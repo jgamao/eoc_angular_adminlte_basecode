ed5ccdf0106363c84d0609c38dc5e90db50cc062
Joselito Gamao Jr <jgamao@Joselitos-MacBook-Pro.local>
2018-04-06 11:48:44 +0800

    Implemented Login Page

--- demo/src/app/+login/login.component.ts
+++ demo/src/app/+login/login.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public user = new User('','');
+  public errorMsg = '';
+    bodyClasses = 'login-page';
+  body: HTMLBodyElement = document.getElementsByTagName('body')[0];
+
+  constructor(private _service:AuthService) { }
 
   ngOnInit() {
+    // add the the body classes
+    this.body.classList.add('login-page');
   }
 
+   ngOnDestroy() {
+    // remove the the body classes
+    this.body.classList.remove('login-page');
+  }
+
+  login() {
+    if (!this._service.login(this.user)){
+      this.errorMsg = 'Failed to login';
+    }
+  }
 }
