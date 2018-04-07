3c9550753b3de607bc0ab434f8ab901a94d5ed76
Joe <gamao.joselito@gmail.com>
2018-04-07 17:36:37 +0800

    Sidebar

--- demo/src/app/admin-lte.conf.ts
+++ demo/src/app/admin-lte.conf.ts
@@ -15,26 +15,26 @@ export var adminLteConf = {
     {label: 'Reports', route: 'reports', iconClasses: 'fa fa-line-chart'},
     {label: 'Site Managers', route: 'site-managers', iconClasses: 'fa fa-id-badge'},
     {label: 'Calendar', route: 'calendar', iconClasses: 'fa fa fa-calendar'},
-    {label: 'Layout', iconClasses: 'fa fa-th-list', children: [
-      {label: 'Configuration', route: 'layout/configuration'},
-      {label: 'Custom', route: 'layout/custom'},
-      {label: 'Header', route: 'layout/header'},
-      {label: 'Sidebar Left', route: 'layout/sidebar-left'},
-      {label: 'Sidebar Right', route: 'layout/sidebar-right'},
-      {label: 'Content', route: 'layout/content'}
-    ]},
-    {label: 'COMPONENTS', separator: true},
-    {label: 'Accordion', route: 'accordion', iconClasses: 'fa fa-tasks'},
-    {label: 'Alert', route: 'alert', iconClasses: 'fa fa-exclamation-triangle'},
-    {label: 'Boxs', iconClasses: 'fa fa-files-o', children: [
-      {label: 'Default Box', route: 'boxs/box'},
-      {label: 'Info Box', route: 'boxs/info-box'},
-      {label: 'Small Box', route: 'boxs/small-box'}
-    ]},
-    {label: 'Dropdown', route: 'dropdown', iconClasses: 'fa fa-arrows-v'},
-    {label: 'Form', iconClasses: 'fa fa-files-o', children: [
-      {label: 'Input Text', route: 'form/input-text'}
-    ]},
-    {label: 'Tabs', route: 'tabs', iconClasses: 'fa fa-th'}
+    // {label: 'Layout', iconClasses: 'fa fa-th-list', children: [
+    //   {label: 'Configuration', route: 'layout/configuration'},
+    //   {label: 'Custom', route: 'layout/custom'},
+    //   {label: 'Header', route: 'layout/header'},
+    //   {label: 'Sidebar Left', route: 'layout/sidebar-left'},
+    //   {label: 'Sidebar Right', route: 'layout/sidebar-right'},
+    //   {label: 'Content', route: 'layout/content'}
+    // ]},
+    // {label: 'COMPONENTS', separator: true},
+    // {label: 'Accordion', route: 'accordion', iconClasses: 'fa fa-tasks'},
+    // {label: 'Alert', route: 'alert', iconClasses: 'fa fa-exclamation-triangle'},
+    // {label: 'Boxs', iconClasses: 'fa fa-files-o', children: [
+    //   {label: 'Default Box', route: 'boxs/box'},
+    //   {label: 'Info Box', route: 'boxs/info-box'},
+    //   {label: 'Small Box', route: 'boxs/small-box'}
+    // ]},
+    // {label: 'Dropdown', route: 'dropdown', iconClasses: 'fa fa-arrows-v'},
+    // {label: 'Form', iconClasses: 'fa fa-files-o', children: [
+    //   {label: 'Input Text', route: 'form/input-text'}
+    // ]},
+    // {label: 'Tabs', route: 'tabs', iconClasses: 'fa fa-th'}
   ]
 };
