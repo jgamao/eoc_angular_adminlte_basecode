45d7d2acaf87babaf4fb704ed43796bb6ac21d99
Joe <gamao.joselito@gmail.com>
2018-04-11 23:42:31 +0800

    Partial Calendar

--- demo/src/app/+calendar/calendar.component.ts
+++ demo/src/app/+calendar/calendar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewChecked } from '@angular/core';
+import { CalendarComponent as CalendarWrapper } from 'ng-fullcalendar';
+import { Options } from 'fullcalendar';
+import { EventService } from '../services/events/event.service';
 
 @Component({
   selector: 'app-calendar',
@@ -7,9 +10,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  calendarOptions: Options;
+  displayEvent: any;
+  events = null;
+  @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
+  constructor(protected eventService: EventService) { }
 
   ngOnInit() {
+    this.calendarOptions = {
+    	selectable: true,
+      editable: true,
+      eventLimit: false,
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listMonth'
+      },
+      events: []
+    };
+  }
+
+  // ngAfterViewChecked() {
+  // 	this.loadevents();
+  // }
+
+  loadevents() {
+    this.eventService.getEvents().subscribe(data => {
+      this.events = data;
+    });
+  }
+
+  clickButton(model: any) {
+    this.displayEvent = model;
+  }
+
+  dayClick(model: any) {
+    console.log('test', model);
+  }
+
+  eventClick(model: any) {
+    model = {
+      event: {
+        id: model.event.id,
+        start: model.event.start,
+        end: model.event.end,
+        title: model.event.title,
+        allDay: model.event.allDay
+        // other params
+      },
+      duration: {}
+    }
+    this.displayEvent = model;
+  }
+
+  updateEvent(model: any) {
+    model = {
+      event: {
+        id: model.event.id,
+        start: model.event.start,
+        end: model.event.end,
+        title: model.event.title
+        // other params
+      },
+      duration: {
+        _data: model.duration._data
+      }
+    }
+    this.displayEvent = model;
   }
 
 }
--- demo/src/app/+calendar/calendar.module.ts
+++ demo/src/app/+calendar/calendar.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FullCalendarModule } from 'ng-fullcalendar';
 
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarComponent } from './calendar.component';
+import { EventService } from '../services/events/event.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    CalendarRoutingModule
+    CalendarRoutingModule,
+    FullCalendarModule
   ],
-  declarations: [CalendarComponent]
+  declarations: [CalendarComponent],
+  providers: [EventService]
 })
 export class CalendarModule { }
